685325abb5d4b0e28e3b524e12e04eeeab460c60
FcoCrespo <Fco.Crespo@alu.uclm.es>
2020-09-23 12:06:55 +0200

    spring boot

---
+++ src/main/java/edu/uclm/esi/devopsmetrics/controllers/TestController.java
@@ -0,0 +1,19 @@
+package edu.uclm.esi.devopsmetrics.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+		
+	@GetMapping("/")
+    public String get0(){
+        return "hola Bienvenido!";
+    }
+    
+    @GetMapping("/hola")
+    public String getHello(){
+        return "hola en hola!";
+    }
+
+}
