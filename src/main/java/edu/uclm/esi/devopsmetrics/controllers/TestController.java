bdfeb6363009c5df26adf7872acc872fd1221e52
FcoCrespo <Fco.Crespo@alu.uclm.es>
2020-09-28 16:25:45 +0200

    prueba variable entorno heroku

--- src/main/java/edu/uclm/esi/devopsmetrics/controllers/TestController.java
+++ src/main/java/edu/uclm/esi/devopsmetrics/controllers/TestController.java
@@ -1,14 +1,20 @@
 package edu.uclm.esi.devopsmetrics.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+	
+	@Autowired
+	private Environment env;
 		
 	@GetMapping("/")
     public String get0(){
-        return "hola Bienvenido!";
+		String property = env.getProperty("spring.datasource.username");
+        return property;
     }
     
     @GetMapping("/hola")
