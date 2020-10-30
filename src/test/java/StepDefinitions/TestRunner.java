4cb9f927d90b1ad95edad81283e1a9e6bcfd1cd6
FcoCrespo <Fco.Crespo@alu.uclm.es>
2020-10-30 19:34:09 +0100

    / changes inm file route

--- src/test/java/StepDefinitions/TestRunner.java
+++ src/test/java/StepDefinitions/TestRunner.java
@@ -50,42 +50,7 @@ public class TestRunner {
     	
     	File fJSONoriginal = new File("C:\\Users\\Crespo\\.jenkins\\workspace\\devopsmetrics\\target\\cucumber\\TestReport.json");
         //File fJSONoriginal = new File("target/cucumber/TestReport.json");
-    	System.out.println("Hola");
-    	if(fJSONoriginal.exists()) {
-    		String dir1 = "C:\\Users\\Crespo\\eclipse-workspace\\devopsmetrics\\target\\cucumberEXISTE"+dtf.format(now);
-
-            try {
-
-                Path path = Paths.get(dir1);
-
-                Files.createDirectories(path);
-
-                System.out.println("Directory is created!");
-
-                //Files.createDirectory(path);
-
-            } catch (IOException e) {
-                System.err.println("Failed to create directory!" + e.getMessage());
-            }
-    	}
-    	else {
-    		String dir2 = "C:\\Users\\Crespo\\eclipse-workspace\\devopsmetrics\\target\\cucumberNOEXISTE"+dtf.format(now);
-
-            try {
-
-                Path path = Paths.get(dir2);
-
-                Files.createDirectories(path);
-
-                System.out.println("Directory is created!");
-
-                //Files.createDirectory(path);
-
-            } catch (IOException e) {
-                System.err.println("Failed to create directory!" + e.getMessage());
-            }
-    	}
-    	File fJSONrenombrado = new File("C:\\Users\\Crespo\\eclipse-workspace\\devopsmetrics\\target/cucumber"+dtf.format(now)+"/TestReport"+dtf.format(now)+".json");
+    	File fJSONrenombrado = new File("C:\\Users\\Crespo\\eclipse-workspace\\devopsmetrics\\target\\cucumber"+dtf.format(now)+"\\TestReport"+dtf.format(now)+".json");
     	
     	//File fJUnitoriginal = new File("target/cucumber/TestReport.xml");
     	File fJUnitoriginal = new File("C:\\Users\\Crespo\\.jenkins\\workspace\\devopsmetrics\\target\\cucumber\\TestReport.xml");
@@ -93,7 +58,7 @@ public class TestRunner {
     	
     	//File fHTMLoriginal = new File("target/cucumber/TestReport.html");
     	File fHTMLoriginal = new File("C:\\Users\\Crespo\\.jenkins\\workspace\\devopsmetrics\\target\\cucumber\\TestReport.html");
-    	File fHTMLrenombrado = new File("C:\\Users\\Crespo\\eclipse-workspace\\devopsmetrics\\target/cucumber"+dtf.format(now)+"/TestReport"+dtf.format(now)+".html");
+    	File fHTMLrenombrado = new File("C:\\Users\\Crespo\\eclipse-workspace\\devopsmetrics\\target\\cucumber"+dtf.format(now)+"\\TestReport"+dtf.format(now)+".html");
     	
     	fJSONoriginal.renameTo(fJSONrenombrado);
     	fJUnitoriginal.renameTo(fJUnitrenombrado);
