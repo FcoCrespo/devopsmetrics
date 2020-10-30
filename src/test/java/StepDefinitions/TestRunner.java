45c8b2028989fea52dc2b63cb3dd9f3fa69fd61d
FcoCrespo <Fco.Crespo@alu.uclm.es>
2020-10-30 17:14:32 +0100

    cucumber test report

--- src/test/java/StepDefinitions/TestRunner.java
+++ src/test/java/StepDefinitions/TestRunner.java
@@ -19,7 +19,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/resources/features", glue="StepDefinitions",
 monochrome =true,
-plugin = {"json:target/JSONReports/TestReport.json","pretty", "junit:target/JUnitReports/TestReport.xml","html:target/HTMLReports/TestReport.html"}
+plugin = {"json:target/cucumber/TestReport.json","pretty", "junit:target/cucumber/TestReport.xml","html:target/cucumber/TestReport.html"}
 		)
 public class TestRunner {
 
@@ -27,12 +27,12 @@ public class TestRunner {
     public static void setupAfter() {
     	DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd-MM-yyyy-HH-mm");  
  	   	LocalDateTime now = LocalDateTime.now();  
-    	File fJSONoriginal = new File("target/JSONReports/TestReport.json");
-    	File fJSONrenombrado = new File("target/JSONReports/TestReport"+dtf.format(now)+".json");
-    	File fJUnitoriginal = new File("target/JUnitReports/TestReport.xml");
-    	File fJUnitrenombrado = new File("target/JUnitReports/TestReport"+dtf.format(now)+".xml");
-    	File fHTMLoriginal = new File("target/HTMLReports/TestReport.html");
-    	File fHTMLrenombrado = new File("target/HTMLReports/TestReport"+dtf.format(now)+".html");
+    	File fJSONoriginal = new File("target/cucumber/TestReport.json");
+    	File fJSONrenombrado = new File("target/cucumber/TestReport"+dtf.format(now)+".json");
+    	File fJUnitoriginal = new File("target/cucumber/TestReport.xml");
+    	File fJUnitrenombrado = new File("target/cucumber/TestReport"+dtf.format(now)+".xml");
+    	File fHTMLoriginal = new File("target/cucumber/TestReport.html");
+    	File fHTMLrenombrado = new File("target/cucumber/TestReport"+dtf.format(now)+".html");
     	fJSONoriginal.renameTo(fJSONrenombrado);
     	fJUnitoriginal.renameTo(fJUnitrenombrado);
     	fHTMLoriginal.renameTo(fHTMLrenombrado);
