88cf7a241de6f1d7581e3af1beb2b261ed84e491
FcoCrespo <Fco.Crespo@alu.uclm.es>
2020-10-30 19:59:15 +0100

    sin nada

--- src/test/java/StepDefinitions/TestRunner.java
+++ src/test/java/StepDefinitions/TestRunner.java
@@ -27,57 +27,5 @@ monochrome =true,
 plugin = {"json:target/cucumber/TestReport.json","pretty", "junit:target/cucumber/TestReport.xml","html:target/cucumber/TestReport.html"}
 		)
 public class TestRunner {
-
-	@BeforeClass
-    public static void setupAfter() {
-    	DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd-MM-yyyy-HH-mm");  
- 	   	LocalDateTime now = LocalDateTime.now(); 
- 	   	
-    	String dir = "C:\\Users\\Crespo\\eclipse-workspace\\devopsmetrics\\target\\cucumber"+dtf.format(now);
-        
-    	try {
-
-            Path path = Paths.get(dir);
-
-            Files.createDirectories(path);
-
-            System.out.println("Directory is created!");
-
-            //Files.createDirectory(path);
-
-        } catch (IOException e) {
-            System.err.println("Failed to create directory!" + e.getMessage());
-        }
-    	
-    	File fJSONoriginal = new File("C:\\Users\\Crespo\\.jenkins\\workspace\\devopsmetrics\\target\\cucumber");
-        //File fJSONoriginal = new File("target/cucumber/TestReport.json");
-    	File fJSONrenombrado = new File("C:\\Users\\Crespo\\eclipse-workspace\\devopsmetrics\\target\\cucumber"+dtf.format(now));
-    	
-    	//File fJUnitoriginal = new File("target/cucumber/TestReport.xml");
-    	File fJUnitoriginal = new File("C:\\Users\\Crespo\\.jenkins\\workspace\\devopsmetrics\\target\\cucumber");
-    	File fJUnitrenombrado = new File("C:\\Users\\Crespo\\eclipse-workspace\\devopsmetrics\\target\\cucumber"+dtf.format(now));
-    	
-    	//File fHTMLoriginal = new File("target/cucumber/TestReport.html");
-    	File fHTMLoriginal = new File("C:\\Users\\Crespo\\.jenkins\\workspace\\devopsmetrics\\target\\cucumber");
-    	File fHTMLrenombrado = new File("C:\\Users\\Crespo\\eclipse-workspace\\devopsmetrics\\target\\cucumber"+dtf.format(now));
-    	
-    	
-    	try {
-    		Thread.sleep(1000);
-    		fJSONoriginal.renameTo(fJSONrenombrado);
-			Thread.sleep(1000);
-			fJUnitoriginal.renameTo(fJUnitrenombrado);
-			Thread.sleep(1000);
-			fHTMLoriginal.renameTo(fHTMLrenombrado);
-			Thread.sleep(1000);
-    	}
-    	/*fJSONoriginal.renameTo(fJSONrenombrado);
-    	fJUnitoriginal.renameTo(fJUnitrenombrado);
-    	fHTMLoriginal.renameTo(fHTMLrenombrado);*/ 
-    	catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
     
 }
